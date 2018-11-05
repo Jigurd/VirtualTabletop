09d50318edde97843b18afd1a056ce92d3cf782a
hakonschia <hakonschia5@gmail.com>
2018-11-05 15:00:47 +0100

    changed something

--- web/handler.go
+++ web/handler.go
@@ -8,6 +8,19 @@ import (
 	"github.com/jigurd/VirtualTabletop/tabletop"
 )
 
+/*
+Removes empty strings from an array
+*/
+func removeEmpty(arr []string) []string {
+	newArr := []string{}
+	for _, val := range arr {
+		if val != "" {
+			newArr = append(newArr, val)
+		}
+	}
+	return newArr
+}
+
 // HandleRoot responds with 404
 func HandleRoot(w http.ResponseWriter, r *http.Request) {
 	tpl, err := template.ParseFiles("index.html")
@@ -64,6 +77,35 @@ func HandlerRegister(w http.ResponseWriter, r *http.Request) {
 		tpl, err := template.ParseFiles("register.html")
 		if err != nil {
 			fmt.Println("Error parsing register.html")
+		}
+
+		err = tpl.Execute(w, nil)
+		if err != nil {
+			fmt.Println("Error executing")
+		}
+	}
+}
+
+/*
+HandlerLogin handles users logging in
+*/
+func HandlerLogin(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		tpl, err := template.ParseFiles("login.html")
+		if err != nil {
+			fmt.Println("Error parsing login.html")
+		}
+
+		err = tpl.Execute(w, nil)
+		if err != nil {
+			fmt.Println("Error executing")
+		}
+
+	default: // hacky shitty solution for when it comes here the first time xd
+		tpl, err := template.ParseFiles("login.html")
+		if err != nil {
+			fmt.Println("Error parsing login.html")
 		}
 
 		err = tpl.Execute(w, nil)

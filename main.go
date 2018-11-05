145c0e265227fa2c17747904021e374bb518086f
Håkon Schia <haakosc@stud.ntnu.no>
2018-11-05 11:54:37 +0100

    listening and stuff

--- main.go
+++ main.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"net/http"
+	"os"
 )
 
 func main() {
+	port, portOk := os.LookupEnv("PORT")
+	if !portOk {
+		port = "8080" // 8080 is used as the default port
+	}
 
 	http.HandleFunc("/", HandleRoot) // return 404
+
+	http.ListenAndServe(":"+port, nil)
 }

58730af137340939f3c0a831fdff153848350208
Mona Lisa <benjaminbergzh@gmail.com>
2018-11-19 20:40:37 +0100

    commiting just in case

--- web/handler.go
+++ web/handler.go
@@ -849,12 +849,4 @@ func HandleI(w http.ResponseWriter, r *http.Request) {
 }
 
 func HandleGameBoard(w http.ResponseWriter, r *http.Request) {
-	/*
-		tpl, err := template.ParseFiles("html/gameboard.html", "html/header.html")
-		if err != nil {
-			fmt.Println("Error reading playerdirectory.html")
-			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-			return
-		}
-	*/
 }
